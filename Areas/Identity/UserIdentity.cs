758f70e4cb15f2e32199ba110aaba71c3f7660b9
Sleepybear <thomas.ucsc@gmail.com>
2023-03-03 18:19:01 -0800

    start custom identity stuff

---
+++ Areas/Identity/UserIdentity.cs
@@ -0,0 +1,12 @@
+﻿using BrewComp.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace BrewComp.Identity;
+
+public class UserIdentity : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public IEnumerable<Competition> Competitions { get; set; }
+}
---
+++ Data/Competition.cs
@@ -0,0 +1,15 @@
+﻿using NodaTime;
+
+namespace BrewComp.Data;
+
+public class Competition
+{
+    public Guid Id { get; init; }
+    public string Name { get; set; }
+    public DateInterval CompetitionDates { get; set; }
+    public DateInterval DropOffDates { get; set; }
+    public DateInterval ShippingDates { get; set; }
+    public DateInterval RegistrationDates { get; set; }
+
+}
+
