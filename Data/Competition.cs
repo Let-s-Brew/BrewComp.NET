50ac7bf7e01c0e38a54dddbffa4eca65659fee5c
Sleepybear <thomas.ucsc@gmail.com>
2023-05-13 11:33:21 -0700

    Add entry limit property

--- Data/Competition.cs
+++ Data/Competition.cs
@@ -9,6 +9,7 @@ public class Competition
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public Guid Id { get; init; }
     public string Name { get; set; } = string.Empty;
+    public int? EntryLimit { get; set; } = null!;
     public DateRange CompetitionDates { get; set; }
     public DateRange? DropOffDates { get; set; } = null!;
     public DateRange? ShippingDates { get; set; } = null!;
